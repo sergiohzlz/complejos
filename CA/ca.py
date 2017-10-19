4c7f093f16fa17b91bd6fd269d82f7b407516d35
Checo <sergiohzlz@gmail.com>
2017-10-19 13:40:12 -0500

    modificación en la recuperación del elemento i-ésimo de la regla de acuerdo a cadenas

--- CA/ca.py
+++ CA/ca.py
@@ -10,7 +10,18 @@ import matplotlib.pyplot as plt
 
 
 class ECA(object):
+    """
+    Clase para implementar un autómata celular unidimensional
+    """
     def __init__(self, regla, its, longitud, ci='random'):
+        """
+        Constructor. Recibe la regla en decimal, número de
+        iteraciones y la longitud del arreglo.
+        ci puede ser 'random' o un vector especificando la
+        condición inicial.
+        Una vez especificado hay que ejecutar la función
+        'ejecuta' para que se lleve a cabo el cómputo.
+        """
         self.iters = its
         self.R     = ('0'*8 + bin(regla)[2:])[-8:]
         self.L     = longitud
@@ -21,6 +32,10 @@ class ECA(object):
             self.ci = ci
         self.M[0] = ci
     def salva(self, fname, titulo=''):
+        """
+        Salva la imagen en png con el nombre
+        fname
+        """
         fig,ax = plt.subplots()
         ax.imshow(self.M)
         ax.set_title(titulo)
@@ -36,10 +51,13 @@ class ECA(object):
         S = []
         for j in range(len(ant)):
             vec = hstack( (ant[(j-1)%L] , ant[j%L], ant[(j+1)%L]) ).astype(int)
-            s = r[int(''.join(map(str, vec)),2)]
+            s = r[7-int(''.join(map(str, vec)),2)]
             S.append(s)
         return S
     def simulacion(self):
+        """
+        Regrsa la simulación
+        """
         return self.M
 
 
