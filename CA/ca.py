4a825ad8876411db49d9bd13129f3c4422246513
Sergio Hernández-López <sergiohzlz@gmail.com>
2017-10-20 21:01:34 -0500

    regla leida correctamente y opciones leidas correctamente del shell

--- CA/ca.py
+++ CA/ca.py
@@ -3,39 +3,24 @@
 
 import sys
 from random import choice
-from numpy import array, dot, zeros, hstack
+from numpy import array, dot, zeros, hstack, ndarray
 import matplotlib as mpl
 mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 
 
 class ECA(object):
-    """
-    Clase para implementar un autómata celular unidimensional
-    """
     def __init__(self, regla, its, longitud, ci='random'):
-        """
-        Constructor. Recibe la regla en decimal, número de
-        iteraciones y la longitud del arreglo.
-        ci puede ser 'random' o un vector especificando la
-        condición inicial.
-        Una vez especificado hay que ejecutar la función
-        'ejecuta' para que se lleve a cabo el cómputo.
-        """
         self.iters = its
-        self.R     = ('0'*8 + bin(regla)[2:])[-8:]
+        self.R     = list(reversed(('0'*8 + bin(regla)[2:])[-8:]))
         self.L     = longitud
-        self.M     = zeros((its, longitud))
+        self.M     = zeros((its, longitud),dtype='int')
         if(ci=='random'):
             ci = array([choice([0,1]) for i in range(longitud)])
-        elif (type(ci)==list) or (type(ci)==numpy.ndarray):
+        elif (type(ci)==list) or (type(ci)==ndarray):
             self.ci = ci
         self.M[0] = ci
     def salva(self, fname, titulo=''):
-        """
-        Salva la imagen en png con el nombre
-        fname
-        """
         fig,ax = plt.subplots()
         ax.imshow(self.M)
         ax.set_title(titulo)
@@ -50,14 +35,11 @@ class ECA(object):
         L = self.L
         S = []
         for j in range(len(ant)):
-            vec = hstack( (ant[(j-1)%L] , ant[j%L], ant[(j+1)%L]) ).astype(int)
-            s = r[7-int(''.join(map(str, vec)),2)]
+            vec = str(ant[(j-1)%L]) + str(ant[j]) + str(ant[(j+1)%L])
+            s = r[int( vec,2 )]
             S.append(s)
         return S
     def simulacion(self):
-        """
-        Regrsa la simulación
-        """
         return self.M
 
 
@@ -68,6 +50,6 @@ if __name__=='__main__':
     lngtd = int(sys.argv[3])
     salida = sys.argv[4]
 
-    ca = ECA(regla, iters, lngtd, iters)
+    ca = ECA(regla, iters, lngtd)
     ca.ejecuta()
     ca.salva(salida)
